846132400cfbb66d96605e2a2cf67926e460324d
shin_seok <pjjpjj2002@naver.com>
2021-03-08 20:24:32 +0900

    upload

--- DatabaseConnect.js
+++ DatabaseConnect.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'gustjr2924!',
+  password : '',
   database : 'fintech210222'
 });
  
